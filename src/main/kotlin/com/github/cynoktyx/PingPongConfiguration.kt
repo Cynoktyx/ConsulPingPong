abe5902aa5143f2239e021b21f80d82250e88259
Lukas Schlüter <lukas.schlueter+gitlab@gmail.com>
2017-01-31 01:41:51 +0100

    Add ip property to configuration

--- src/main/kotlin/com/github/cynoktyx/PingPongConfiguration.kt
+++ src/main/kotlin/com/github/cynoktyx/PingPongConfiguration.kt
@@ -3,6 +3,8 @@ package com.github.cynoktyx
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.dropwizard.Configuration
 import io.dropwizard.client.JerseyClientConfiguration
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class PingPongConfiguration : Configuration() {
 	lateinit var name: String
@@ -19,4 +21,14 @@ class PingPongConfiguration : Configuration() {
 		internal set
 	var adminPort = 0
 		internal set
+	
+	val ip: String by lazy {
+		val p = Runtime.getRuntime().exec("hostname -i")
+		p.waitFor()
+
+		val reader = BufferedReader(InputStreamReader(p.inputStream))
+		val line = reader.readLine()
+
+		line ?: "127.0.0.1"
+	}
 }
