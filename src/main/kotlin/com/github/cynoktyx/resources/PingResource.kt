139a653d8186f2c62455eac8e5bf1dde1b29d08c
Lukas Schlüter <lukas.schlueter+gitlab@gmail.com>
2017-02-02 23:39:39 +0100

    Add option to ping a specific service id

--- src/main/kotlin/com/github/cynoktyx/resources/PingResource.kt
+++ src/main/kotlin/com/github/cynoktyx/resources/PingResource.kt
@@ -1,11 +1,13 @@
 package com.github.cynoktyx.resources
 
 import com.github.cynoktyx.consul.ConsulConnection
+import com.orbitz.consul.model.health.ServiceHealth
 import java.util.stream.Collectors
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.client.Client
 import javax.ws.rs.client.Entity
 import javax.ws.rs.core.MediaType
@@ -20,8 +22,14 @@ class PingResource @Inject constructor(private val consulConnection: ConsulConne
 	@GET
 	@Path("/ping")
 	@Produces(MediaType.TEXT_PLAIN)
-	fun ping(): Response {
-		return Response.ok(Entity.text("Pong from " + consulConnection.fullServiceName).entity).build()
+	fun ping(@QueryParam("id") id: String?): Response {
+		if (id == null || id == consulConnection.serviceId.toString())
+			return Response.ok(Entity.text("Pong from " + consulConnection.fullServiceName).entity).build()
+
+		val services = consulConnection.getHealthyServicesByName()
+		val service = services.first { it.service.id == id }
+
+		return getRootPath(service).path("/pingPong/ping").request(MediaType.TEXT_PLAIN_TYPE).get()
 	}
 
 	@GET
@@ -31,11 +39,14 @@ class PingResource @Inject constructor(private val consulConnection: ConsulConne
 		val services = consulConnection.getHealthyServicesByName()
 
 		return services.parallelStream().map {
-			client.target("http://" + it.service.address + ":" + it.service.port)
-					.path(consulConnection.getValueAsString("rootPath").or { "/api" })
+			getRootPath(it)
 					.path("/pingPong/ping")
 					.request(MediaType.TEXT_PLAIN_TYPE)
 					.get(String::class.java)
 		}.collect(Collectors.toList<String>())
 	}
+
+	fun getRootPath(service: ServiceHealth) = client.target(
+			"http://" + service.service.address + ":" + service.service.port)
+			.path(consulConnection.getValueAsString("rootPath").or { "/api" })!!
 }
